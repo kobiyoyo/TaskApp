da1d7668e811eb9cd4e81c0bbbced58d388dfcd8
kobiyoyo <adamachubi@gmail.com>
2020-04-21 08:43:59 +0100

    added validation for user , groups and task

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :groups
   has_many :tasks, foreign_key: 'author_id', dependent: :destroy
   has_one_attached :avatar
-  validates :username, presence: true
+  validates :username, presence: true,length: { in: 5..15 }
 end
